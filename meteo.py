568ec0d06025a25c2e6e47a54c64541840aff33f
Mark Laufersweiler <laufers@gmail.com>
2012-03-19 14:31:27 -0500

    Added missing data handler on read and cleaned up variable dictioanries.

--- meteo.py
+++ meteo.py
@@ -1,5 +1,7 @@
 import numpy as np
 # alias for numpy
+from numpy.lib.recfunctions import append_fields
+# needed to import more functionality from numpy
 import matplotlib.pyplot as plt
 # imports matlab like ploting and alaised
 
@@ -18,42 +20,63 @@ def dewpoint(rh, tmpc):
 #x = np.genfromtxt('20120229nrmn.mts',dtype = None, names = True, skip_header = 2)
 
 # time,relh,tmpc,wspd,pres = np.loadtxt('20120302nrmn.mts', skiprows = 3, usecols = [2,3,4,5,12], unpack = True)
-data = np.genfromtxt('20120317nrmn.mts', skiprows = 2 , dtype= None, names = True)
+# data read now includes test for missing data (-996,-995) as used by the OklaMesonet
+data = np.genfromtxt('20120317nrmn.mts', skiprows = 2 , dtype= None, names = True, missing_values = {None:["-996","-995"]}, usemask = True)
 
 
 # post processing of data to facilitate plotting, this ignores missing data and uses the standard NaN(not a number)
 #relh[relh==-996] = np.nan
 
 # second method to mask(remove, ignore) data in preperation for display or analytics.
-relh = np.ma.array(data["RELH"], mask = (data["RELH"] == -996))
-pres = np.ma.array(data["PRES"], mask = (data["PRES"] == -996))
-tmpc = np.ma.array(data["TAIR"], mask = (data["TAIR"] == -996))
-wspd = np.ma.array(data["WSPD"], mask = (data["WSPD"] == -996))
-tmpf = tmp2f(tmpc)
-dptf = tmp2f(dewpoint(relh, tmpc))
+
+DPTF = tmp2f(dewpoint(data["RELH"], data["TAIR"]))
+data["TAIR"] = tmp2f(data["TAIR"])
+
+# coombined function call result to original data array to keep varable data in one structure
+data = append_fields ( data, "DPTF", DPTF)
+
 
 fig = plt.figure()
 
 # Set up of dictionary for use of strings and variable names
 # Strings first owing to non hashing of arrays
 # tuple added to include color
-varname = {'Rel. Humidity' : (relh,'green'),
-		   'Dewpoint F'	   : (dptf, 'green'), 
-		   'Pressure mb' : (pres,'black'),
-#		   'Temperature C' : (tmpc,'red'), 
-		   'Windspeed knots' : (wspd, 'blue'),
-		   'Temperature F' : (tmpf,'red'),}
+# varname = {'Rel. Humidity' : (relh,'green'),
+# 		   'Dewpoint F'	   : (dptf, 'green'), 
+# 		   'Pressure mb' : (pres,'black'),
+# 		   'Temperature C' : (tmpc,'red'), 
+# 		   'Windspeed knots' : (wspd, 'blue'),
+# 		   'Temperature F' : (tmpf,'red'),}
+varPlotColor = dict(TAIR = 'red',
+                    RELH = 'green',
+                    WSPD = 'blue',
+                    WDIR = 'blue',
+                    RAIN = 'blue',
+                    DPTF = 'green',
+                    PRES = 'black',
+                    SRAD = 'dark yellow',
+                    TIME = 'black')
+
+varPlotLabel = dict(TAIR = 'Temperature C',
+                    RELH = 'Rel Humidity %',
+                    WSPD = 'Wind Speed kts',
+                    WDIR = 'Wind Direction',
+                    RAIN = 'Rain 24 hour Accum ins',
+                    DPTF = 'Dewpoint F',
+                    PRES = 'Pressure mb',
+                    SRAD = 'Solar Insolation W/m2',
+                    TIME = 'Minutes after midnight local (CST)')
+                   
 
 # Needed prior to the creation of the dictionary
-# pltlist = [relh, pres, tmpc, wspd]
+pltlist = ["TAIR", "DPTF", "PRES", "WSPD", "RAIN"]
 
 # careful when calling dictionary that tuple is split out and in the case of the y axis
 # remember to keep the dictionary aray
-for panel,metvar in enumerate(varname, start=1):
-    plt.subplot(len(varname),1,panel)
-    var,colr = varname[metvar]
-    plt.plot(data['TIME'], var, colr)
-    plt.xlabel('Mintues after 00z')
-    plt.ylabel(metvar)
+for panel,metvar in enumerate(pltlist, start=1):
+    plt.subplot(len(pltlist),1,panel)
+    plt.plot(data['TIME'], data[metvar], varPlotColor[metvar])
+    plt.xlabel(varPlotLabel["TIME"])
+    plt.ylabel(varPlotLabel[metvar])
 plt.show()
 
